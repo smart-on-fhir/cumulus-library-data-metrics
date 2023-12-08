9ed3dc0d6fefe23d09aa518eedf8562292fa2676
Michael Terry <michael.terry@childrens.harvard.edu>
2023-12-08 14:46:02 -0500

    q_term_use: look for snomed for allergies, whoops

--- quality/q_term_use.py
+++ quality/q_term_use.py
@@ -21,13 +21,12 @@ class TermUseBuilder(MetricMixin, BaseTableBuilder):
     def prepare_queries(self, *args, **kwargs) -> None:
         # https://github.com/sync-for-science/qualifier/blob/master/metrics.md#q_term_use-conformance-expect-common-terminology-systems-to-be-populated
         self.queries = [
-            self.make_table(src="AllergyIntolerance", field="code", system="http://loinc.org"),
+            self.make_table(src="AllergyIntolerance", field="code", system="http://snomed.info/sct"),
             self.make_table(src="Condition", field="code", system="http://snomed.info/sct"),
             self.make_table(src="Device", field="type", system="http://snomed.info/sct"),
             self.make_table(src="DocumentReference", field="type", system="http://loinc.org"),
             self.make_table(src="Immunization", field="vaccineCode", system="http://hl7.org/fhir/sid/cvx"),
             self.make_table(src="Medication", field="code", system="http://www.nlm.nih.gov/research/umls/rxnorm"),
-            # self.make_table(src="MedicationAdministration", field="medicationCodeableConcept", system="http://www.nlm.nih.gov/research/umls/rxnorm"),
             self.make_table(src="MedicationRequest", field="medicationCodeableConcept", system="http://www.nlm.nih.gov/research/umls/rxnorm"),
             self.make_table(src="Observation", field="code", system="http://loinc.org"),
             self.make_table(src="Observation", field="valueCodeableConcept", system="http://snomed.info/sct"),
