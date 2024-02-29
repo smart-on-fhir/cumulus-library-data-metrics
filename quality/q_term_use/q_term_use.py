5ff46033a20ce5dba7683b3cc9a73501a5272bf6
Michael Terry <michael.terry@childrens.harvard.edu>
2024-02-29 09:09:28 -0300

    q_term_use: allow multiple systems
    Also:
    - Improve the percentage rendering for q_* summaries by multiplying
      by 100 and adding a '%' to clarify that it's a percentage and reduce
      some mental math
    - Fix c_pt_count syntax for Athena
    - Add missing c_term_coverage_condition_code table to export list

--- quality/q_term_use/q_term_use.py
+++ quality/q_term_use/q_term_use.py
@@ -11,7 +11,7 @@ from quality.base import MetricMixin
 class TermUseBuilder(MetricMixin, BaseTableBuilder):
     name = "q_term_use"
 
-    def make_table(self, **kwargs) -> str:
+    def make_table(self, **kwargs) -> None:
         """Make a single metric table"""
         summary_key = f"{kwargs['src'].lower()}_{kwargs['field'].lower()}"
         self.summary_entries[summary_key] = None
@@ -20,15 +20,48 @@ class TermUseBuilder(MetricMixin, BaseTableBuilder):
 
     def prepare_queries(self, *args, **kwargs) -> None:
         # https://github.com/sync-for-science/qualifier/blob/master/metrics.md#q_term_use-conformance-expect-common-terminology-systems-to-be-populated
-        self.make_table(src="AllergyIntolerance", field="code", system="http://snomed.info/sct"),
-        self.make_table(src="Condition", field="code", system="http://snomed.info/sct"),
-        self.make_table(src="Device", field="type", system="http://snomed.info/sct"),
-        self.make_table(src="DiagnosticReport", field="code", system="http://loinc.org"),
-        self.make_table(src="DocumentReference", field="type", system="http://loinc.org"),
-        self.make_table(src="Immunization", field="vaccineCode", system="http://hl7.org/fhir/sid/cvx"),
-        self.make_table(src="Medication", field="code", system="http://www.nlm.nih.gov/research/umls/rxnorm"),
-        self.make_table(src="MedicationRequest", field="medicationCodeableConcept", system="http://www.nlm.nih.gov/research/umls/rxnorm"),
-        self.make_table(src="Observation", field="code", system="http://loinc.org"),
-        self.make_table(src="Observation", field="valueCodeableConcept", system="http://snomed.info/sct"),
-        self.make_table(src="Procedure", field="code", system="http://www.ama-assn.org/go/cpt"),
+        # With some differences:
+        # - Allow multiple systems (pulled from US Core v4 profile recommendations)
+        self.make_table(src="AllergyIntolerance", field="code", systems=[
+            "http://snomed.info/sct",
+            "http://www.nlm.nih.gov/research/umls/rxnorm",
+        ]),
+        self.make_table(src="Condition", field="code", systems=[
+            "http://hl7.org/fhir/sid/icd-10-cm",
+            "http://hl7.org/fhir/sid/icd-9-cm",
+            "http://snomed.info/sct",
+        ]),
+        self.make_table(src="Device", field="type", systems=["http://snomed.info/sct"]),
+        self.make_table(src="DiagnosticReport", field="code", systems=["http://loinc.org"]),
+        self.make_table(src="DocumentReference", field="type", systems=["http://loinc.org"]),
+        self.make_table(src="Immunization", field="vaccineCode", systems=[
+            # The FHIR spec also gives urn:oid:1.2.36.1.2001.1005.17 as an example, but the US Core
+            # profile drops that suggestion in favor of only CVX.
+            "http://hl7.org/fhir/sid/cvx",
+        ]),
+        self.make_table(src="Medication", field="code", systems=[
+            # The FHIR spec gives SNOMED as an example, but the US Core profile drops that
+            # suggestion in favor of only RxNorm
+            "http://www.nlm.nih.gov/research/umls/rxnorm",
+        ]),
+        self.make_table(src="MedicationRequest", field="medicationCodeableConcept", systems=[
+            # The FHIR spec gives SNOMED as an example, but the US Core profile drops that
+            # suggestion in favor of only RxNorm
+            "http://www.nlm.nih.gov/research/umls/rxnorm",
+        ]),
+        self.make_table(src="Observation", field="code", systems=["http://loinc.org"]),
+        self.make_table(src="Observation", field="valueCodeableConcept", systems=[
+            # Base FHIR doesn't suggest anything specific here, but the Laboratory and
+            # Smoking Status profiles both want SNOMED.
+            "http://snomed.info/sct",
+        ]),
+        self.make_table(src="Procedure", field="code", systems=[
+            # Base FHIR only gives SNOMED as an example. But the US Core v4 profile lists all these.
+            "http://loinc.org",
+            "http://snomed.info/sct",
+            "http://www.ada.org/cdt",
+            "http://www.ama-assn.org/go/cpt",
+            "https://www.cms.gov/Medicare/Coding/HCPCSReleaseCodeSets",
+            "http://www.cms.gov/Medicare/Coding/ICD10",
+        ]),
         self.make_summary()
