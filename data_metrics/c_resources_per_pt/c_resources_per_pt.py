7abb120b366587cb1772c0a24f6555eea2601c79
Michael Terry <michael.terry@childrens.harvard.edu>
2024-05-08 16:11:37 -0400

    c_resources_per_pt: add slicing by category, and a max column

--- data_metrics/c_resources_per_pt/c_resources_per_pt.py
+++ data_metrics/c_resources_per_pt/c_resources_per_pt.py
@@ -1,8 +1,9 @@
 """Module for generating c_resources_per_pt tables"""
 
 from cumulus_library.base_table_builder import BaseTableBuilder
-from data_metrics.base import MetricMixin
 from data_metrics import resource_info
+from data_metrics.base import MetricMixin
+
 
 class ResourcesPerPatientBuilder(MetricMixin, BaseTableBuilder):
     name = "c_resources_per_pt"
@@ -10,4 +11,12 @@ class ResourcesPerPatientBuilder(MetricMixin, BaseTableBuilder):
     def add_metric_queries(self) -> None:
         # https://github.com/sync-for-science/qualifier/blob/master/metrics.md#c_resources_per_pt-volume-distribution-of-unique-resources-per-patient-by-resource-type-by-category
 
-        self.queries.append(self.render_sql(self.name, resources=resource_info.PATIENTS))
+        # The SQL wants to examine all the resources at once - so we don't do our normal metric
+        # pattern of rendering the template once per resource.
+        self.queries += [
+            self.render_sql(
+                self.name,
+                patient_fields=resource_info.PATIENTS,
+                categories=resource_info.CATEGORIES,
+            ),
+        ]
