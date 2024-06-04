1ed1158733271e8841865a0b201fe4ef45fe2c9f
Michael Terry <michael.terry@childrens.harvard.edu>
2024-06-04 11:11:19 -0400

    c_us_core_v4_count: skip duplicated fields like valid_verification_status

--- cumulus_library_data_metrics/c_us_core_v4_count/c_us_core_v4_count.py
+++ cumulus_library_data_metrics/c_us_core_v4_count/c_us_core_v4_count.py
@@ -9,6 +9,14 @@ class UsCoreV4CountBuilder(UsCoreV4Mixin, BaseTableBuilder):
     name = "c_us_core_v4_count"
 
     def make_table(self, **kwargs) -> None:
+        # Some checks are duplicated between mandatory and must-support.
+        # For example, some required binding checks are done on the mandatory side
+        # for fields that are marked as must-support. And when checking must-support
+        # validity, we always check binding there too. So this flags the jinja
+        # to skip such duplicated checks, which might be confusing if you're looking
+        # at a metric like this one with both mandatory and must-support fields.
+        kwargs["skip_duplicated_mandatory_checks"] = True
+
         if self.get_output_mode() == "cube" and "mandatory_split" in kwargs:
             kwargs["table_max"] = kwargs["mandatory_split"]
             self.queries += [
