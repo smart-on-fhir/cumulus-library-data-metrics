5c6f1dc41f49e5bec7ff5af886bd14c6011c50b1
Michael Terry <michael.terry@childrens.harvard.edu>
2024-10-25 10:19:57 -0400

    Update for Library 4.0
    - Bump version to 6.0
    - Uses a dynamic study prefix (which is currently staticly defined).
      I just wanted to get this machinery in place so that there is less
      code conflicts as we move forward with other changes.
    - Update ruff.
    - Use new [file_config] section.

--- cumulus_library_data_metrics/__init__.py
+++ cumulus_library_data_metrics/__init__.py
@@ -1,3 +1,3 @@
 """Data Metrics study for Cumulus Library"""
 
-__version__ = "5.1.0"
+__version__ = "6.0.0"
---
+++ cumulus_library_data_metrics/gen_prefix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# TODO: once we implement study-cohort slicing,
+#  uncomment this file to allow writing to a study-specific prefix.
+#  But for now, just always use boring old data_metrics__.
+print("data_metrics")
+
+# import argparse
+#
+# parser = argparse.ArgumentParser()
+# parser.add_argument("--study")
+# args, _rest = parser.parse_known_args()
+#
+# if args.study:
+#     print(f"data_metrics_{args.study}")
+# else:
+#     print("data_metrics")
